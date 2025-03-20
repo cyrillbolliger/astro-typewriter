6125f7452ce2eb3dcffe300849d5cdda61f7767c
Cyrill Bolliger <mail@cyrill.me>
2025-03-20 19:28:04 +0100

    [BUILD] add eslint-plugin-compat with browserslist config

--- packages/astro-typewriter/eslint.config.mjs
+++ packages/astro-typewriter/eslint.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+import compat from "eslint-plugin-compat";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   eslintPluginAstro.configs.recommended,
+  compat.configs["flat/recommended"]
 );
