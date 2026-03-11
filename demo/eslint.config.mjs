1204471a89caafcf11e3e6665c78c9c3cddd675b
Cyrill Bolliger <mail@cyrill.me>
2026-03-11 21:29:36 +0100

    [FEAT] support for astro 6

--- demo/eslint.config.mjs
+++ demo/eslint.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 import compat from "eslint-plugin-compat";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
